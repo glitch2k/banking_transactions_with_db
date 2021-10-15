2abf6d43dd8d5b0e2eea12351338ed5a3015212e
glitch2k <saniltison@outlook.com>
2021-10-15 14:49:12 -0400

    The 2 required endpoints are complete - the code can do the following:     - receive a GET request on an endpoint to retreive the member     information for 1 member using their member id
        - receive a GET request on an endpoint to retreive the total amount
        of money in the back

--- api_to_interact_with_bank_db.py
+++ api_to_interact_with_bank_db.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+import jsonify
 
 app = Flask(__name__)
 api = Api(app)
@@ -22,7 +23,7 @@ class Member(db.Model):
     # this create a user-friendly string that will display to describe
     # ...a record from the database if it is called via code
 	# def __repr__(self):
-	# 	return f"Video(name = {name}, views = {views}, likes = {likes})"
+	# 	return f"member(name = {name}, views = {views}, likes = {likes})"
 
 # YOU NEED TO RUN THE FUNCTION TO CREATE THE DATABASE!!!!!!!
 # comment this line after the database has been created
@@ -32,72 +33,55 @@ class Member(db.Model):
 
 # this dictionary will be used in conjunction with the "@marshal_with" module
 # it defines how that module will convert an object into a serialized json object
-resource_fields = {
+mem_info_return = {
 	'id': fields.Integer,
 	'fname': fields.String,
 	'lname': fields.String,
 	'chk_bal': fields.Integer
 }
 
-# this boc will parse thru the parameters sent in the PUT request to make sure that
-# ...they are valid entries
-# it even sends a custom error message when an invalid parameter is sent
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
-video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
-video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
+total_bal_return = {
+	'bank_bal': fields.Integer
+}
 
-# this creates an api endpoint to retreive data from the database based on the video id
+
+# this creates an api endpoint to retreive data from the database based on the member id
 # ...that was passed in the url
-class Video(Resource):
-	@marshal_with(resource_fields)
-	def get(self, video_id): # "video_id" will hold the value to perform the query with
-		result = Member.query.filter_by(id=video_id).first() # this will return an object
+class MemInfo(Resource):
+	@marshal_with(mem_info_return)
+	def get(self, mem_id): # "mem_id" will hold the value to perform the query with
+		result = Member.query.filter_by(id=mem_id).first() # this will return an object
 		if not result:
-			abort(404, message="Could not find video with that id")
-		return result
+			abort(404, message="Could not find member with that id")
+		return result.lname
+
+class BankBalance(Resource):
+	# bank_bal = 0
+	# @marshal_with(total_bal_return)
+	def get(self):
+		bank_bal = 0
+		accts = Member.query.all()
+		# result = Member.query.filter_by(id=1).first()
+		for acct in accts:
+			bank_bal = acct.chk_bal + bank_bal
+		return {"total bank balance": bank_bal}
 
 
-	@marshal_with(resource_fields)
-	def put(self, video_id):
-		
-		# this boc will parse thru the parameters sent in the PUT request to make sure that
-		# ...they are valid entries
-		# it even sends a custom error message when an invalid parameter is sent
-		video_put_args = reqparse.RequestParser()
-		video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
-		video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
-		video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
-
-		# once all are parameters are validated, the resultant parameters are saved in the "args"
-		# ...variable as an object "most likely a python dict"
-		args = video_put_args.parse_args()
-
-		# this boc queries the db to verify if the record that needs to be created does not exist
-		# ...in the db
-		# if it is not present, the PUT request is aborted and a custom message is sent back
-		result = Member.query.filter_by(id=video_id).first()
-		if result:
-			abort(409, message="Video id is already taken... Choose another id...")
-
-		# once it is verified that the record does not exist, a sqlalchemy object representing the
-		# ...record is created using the "Member" class
-		# the properties for that object will come from the "args" object
-		# the sqlalchemy object representing the record is saved in the variable "video"
-		video = Member(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
-		# the record is sent to the db with the "db.session.add" method with the "video" object used as
-		# ...a parameter of that method
-		db.session.add(video)
-		db.session.commit()
-		return video, 201
+	# def get(self):
+    #     accts = Member.query.all()
+	# 	for acct in accts:
+    #     	bank_bal = acct.chk_bal + bank_bal
+    #     # result = jsonify(accts)
+    #     return bank_bal
 
 
 # this ties the api endpoint to the a pattern that will be received in from the url request
 # when the api receives a GET request with the following url:
-#   - http://127.0.0.0:5000/video/2
-# it will envoke the "Video" class and pass the parameters from the request into that class for 
+#   - http://127.0.0.0:5000/member/2
+# it will envoke the "member" class and pass the parameters from the request into that class for 
 # ...further processing 
-api.add_resource(Video, "/video/<int:video_id>")
+api.add_resource(MemInfo, "/member/<int:mem_id>")
+api.add_resource(BankBalance, "/bank_bal")
 
 if __name__ == "__main__":
 	app.run(debug=True)
